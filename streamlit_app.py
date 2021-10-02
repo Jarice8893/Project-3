edc11572753d3025c12a048b60e7e31f6ff70ad9
Jarice Marshall <jmarshall@homeaccess.com>
2021-10-01 19:59:54 -0500

    streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
+from os import lseek
 import streamlit as st
 from datetime import date
 import yfinance as yf
-
 from plotly import graph_objs as go
 
 START = "1980-12-12"
@@ -9,8 +9,20 @@ TODAY = date.today().strftime("%Y-%m-%d")
 
 st.title("Stock prediction")
 
-stocks = ("AAPL", "MSFT")
+stocks = ("AAPL", "MSFT", "AP", "SPY", "TSLA", "USO")
 selected_stocks = st.selectbox("Select Dataset for prediction", stocks)
 
 n_years = st.slider("Years of Prediction", 1, 4)
 prediction = n_years * 365
+
+def load_data(ticker):
+    data = yf.download(ticker, START, TODAY)
+    data.rest_index(inplace=True)
+    return data
+
+data_load_state = st.text("Load data..")
+data = load_data(selected_stocks)
+data_load_state.text("Loading Data... done!")
+
+
+
